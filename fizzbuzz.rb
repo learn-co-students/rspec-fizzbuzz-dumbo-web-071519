76d3a0764938eb1bb642782e8e118a71a0c410da
Ashna Shah-Grover <as19@williams.edu>
2019-05-09 20:15:31 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    return "Fizz"
+  if int % 15 == 0
+    return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 15 == 0
-    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
   else 
     return nil
+  end
 end
